e6e90cfae800ae656f418179be755f853902c4f3
CrockerUC <crockecf@mail.uc.edu>
2026-03-23 20:41:54 -0400

    Part 2 Finished Made random exclude 0

--- src/ArrayStuff.java
+++ src/ArrayStuff.java
@@ -9,10 +9,12 @@ public class ArrayStuff
         Random random = new Random();
 
         int[] dataPoints = new int[100];
+
         for(int i = 0; i < dataPoints.length; i++)
         {
-            dataPoints[i] = random.nextInt(100);
+            dataPoints[i] = random.nextInt(100) + 1;
         }
+
         for(int i = 0; i < dataPoints.length; i++)
         {
             System.out.print(dataPoints[i] + " | ");
@@ -27,5 +29,39 @@ public class ArrayStuff
         double average = (double)sum / dataPoints.length;
         System.out.println("The sum of dataPoints is: " + sum);
         System.out.println("The average of dataPoints is: " + average);
+
+        int userValue = SafeInput.getRangedInt(in, "Enter value to search", 1, 100);
+
+        int count = 0;
+        for(int i = 0; i < dataPoints.length; i++)
+        {
+            if(dataPoints[i] == userValue) count++;
+        }
+        System.out.println("The value " + userValue + " appears " + count + " times.");
+
+        int searchValue = SafeInput.getRangedInt(in, "Enter value to search for", 1, 100);
+        int index = -1;
+        boolean found = false;
+        for(int i = 0; i < dataPoints.length; i++)
+        {
+            if(dataPoints[i] == searchValue)
+            {
+                index = i;
+                found = true;
+                break;
+            }
+        }
+        if(found) System.out.println("The value " + searchValue + " was found at array index " + index);
+        else System.out.println("The value " + searchValue + " was not found.");
+
+        int min = dataPoints[0];
+        int max = dataPoints[0];
+        for(int i = 1; i < dataPoints.length; i++)
+        {
+            if(dataPoints[i] < min) min = dataPoints[i];
+            if(dataPoints[i] > max) max = dataPoints[i];
+        }
+        System.out.println("The minimum value is: " + min);
+        System.out.println("The maximum value is: " + max);
     }
 }
