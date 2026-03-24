ad2484aeb5f4b24c86d8c5ad33ac5b6afc2a565b
CrockerUC <crockecf@mail.uc.edu>
2026-03-23 20:23:16 -0400

    Part 1 Finished

--- src/ArrayStuff.java
+++ src/ArrayStuff.java
@@ -1,7 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class ArrayStuff
 {
     public static void main(String[] args)
     {
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
 
+        int[] dataPoints = new int[100];
+        for(int i = 0; i < dataPoints.length; i++)
+        {
+            dataPoints[i] = random.nextInt(100);
+        }
+        for(int i = 0; i < dataPoints.length; i++)
+        {
+            System.out.print(dataPoints[i] + " | ");
+        }
+        System.out.println();
+
+        int sum = 0;
+        for(int i = 0; i < dataPoints.length; i++)
+        {
+            sum += dataPoints[i];
+        }
+        double average = (double)sum / dataPoints.length;
+        System.out.println("The sum of dataPoints is: " + sum);
+        System.out.println("The average of dataPoints is: " + average);
     }
 }
